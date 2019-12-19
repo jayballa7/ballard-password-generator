372ca7c77b4d9b8aabd2eb7406542a50878be7a9
Jenn <jenn_ballard7@hotmail.com>
2019-12-19 04:52:44 -0800

    began js script

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
+// different data types have arrays
 
 
+// Special characters array loop //
+var Special = ['~','!','@','#','$','%','^','&','*'];
+var arrayLength = Special.length;
+for (var i=0; i < arrayLength; i++) {
+    console.log(Special[1]);
+}
+// Numeric characters array loop //
+var Numeric = ['0','1','2','3','4','5','6','7','8','9'];
+var arrayLength = Numeric.length;
+for (var i=0; i < arrayLength; i++) {
+    console.log(Numeric[i]);
+}
+// Lowercase characters array loop // 
+var Lowercase = ['a', 'b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var arrayLength = Lowercase.length;
+for (var i=0; i < arrayLength; i++) {
+    console.log(Lowercase[i]);
+}
+// Uppercase characters array loop //
+var Uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var arrayLength = Uppercase.length;
+for (var i=0; i < arrayLength; i++) {
+    console.log(Uppercase[i]);
+}
